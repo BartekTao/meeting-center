a7bc50060fe0cc8188e29ac8cb7d26c1d4696b8f
Bartek <pony830906@gmail.com>
2024-03-27 00:29:22 +0800

    feat: inject mogoClient and meetingManger to resolver

--- cmd/server.go
+++ cmd/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
@@ -9,6 +10,8 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/BartekTao/nycu-meeting-room-api/internal/graph"
 	"github.com/BartekTao/nycu-meeting-room-api/internal/graph/resolvers"
+	infra "github.com/BartekTao/nycu-meeting-room-api/internal/infrastructure"
+	"github.com/BartekTao/nycu-meeting-room-api/internal/meeting"
 )
 
 const defaultPort = "8080"
@@ -19,7 +22,31 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &resolvers.Resolver{}}))
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		log.Fatal("You must set the MONGO_URI environment variable")
+	}
+	ctx := context.Background()
+
+	mongoClient, err := infra.NewMongoDBClient(ctx, infra.MongoDBConfig{URI: mongoURI})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer func() {
+		if err := mongoClient.Disconnect(ctx); err != nil {
+			log.Panic(err)
+		}
+	}()
+	err = mongoClient.Ping(ctx, nil)
+	if err != nil {
+		log.Panic("Failed to ping MongoDB:", err)
+	}
+
+	log.Println("Successfully connected and pinged MongoDB.")
+
+	meetingManager := meeting.NewBasicMeetingManager(mongoClient)
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: resolvers.NewResolver(meetingManager)}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
--- internal/graph/resolvers/resolver.go
+++ internal/graph/resolvers/resolver.go
@@ -1,7 +1,17 @@
 package resolvers
 
+import "github.com/BartekTao/nycu-meeting-room-api/internal/meeting"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	meetingManager meeting.MeetingManager
+}
+
+func NewResolver(meetingManager meeting.MeetingManager) *Resolver {
+	return &Resolver{
+		meetingManager: meetingManager,
+	}
+}
