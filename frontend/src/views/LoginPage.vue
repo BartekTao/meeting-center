0410241bc5fdbca3bd86a4a7d9efdb650747a146
Ivae Lee <leeivan1007@gmail.com>
2024-06-03 13:06:33 +0800

    Added the login-page

---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="login-page">
+      <h1>登入</h1>
+      <button @click="loginWithGoogle">Google 登入</button>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'LoginPage',
+    methods: {
+    async loginWithGoogle() {
+            try {
+                window.location.href = 'http://localhost:8080/auth/google/login';
+            } catch (error) {
+                console.error('登入失敗:', error);
+            }
+        }
+    },
+  }
+  </script>
+  
+  <style scoped>
+  .login-page {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  button {
+    padding: 10px 20px;
+    font-size: 16px;
+  }
+  </style>
